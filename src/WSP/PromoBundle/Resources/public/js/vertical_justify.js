e10cf9f797264b6894e35fbe1ec97cc3a107e735
Ephraim <ephraim.pepe@gmail.com>
2013-12-30 12:47:39 +0100

    Fix js

--- src/WSP/PromoBundle/Resources/public/js/vertical_justify.js
+++ src/WSP/PromoBundle/Resources/public/js/vertical_justify.js
@@ -2,18 +2,24 @@ var VerticalJustify = function() {
 
     var windows_height = 0;
     var tot_height = 0;
+    var margin = 0;
+    var div_logo = null;
 
     return {
         //main function to initiate the module
         init: function() {
+            div_logo = $('#logo').closest('div');
+            margin = parseInt(div_logo.css('padding-top')) + parseInt(div_logo.css('margin-top'));
+            tot_height = $('#logo').height() + parseInt(div_logo.css('padding-top')) + parseInt(div_logo.css('margin-top')) + 
+                         $('#content').height() + parseInt($('#content').css('padding-bottom')) + parseInt($('#content').css('margin-bottom')) +
+                         $('#countdown').height() + parseInt($('#countdown').css('padding-top')) + parseInt($('#countdown').css('margin-top'));
             this._giustifica();
             $(window).resize(this._giustifica);
         },
         _giustifica: function() {
             windows_height = $(window).height();
-            tot_height = ($('#logo').height() + $('#content').height() + $('#countdown').height()) + 200;
             if(tot_height < windows_height) {
-                $('#content').css('margin-top', (windows_height - tot_height) / 2 + 'px');
+                $('#content').css('margin-top', (windows_height - tot_height - margin) + 'px');
             }
         }
     };
