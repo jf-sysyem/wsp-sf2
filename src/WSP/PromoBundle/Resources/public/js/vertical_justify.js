033fb4ef1dcc4ff34ee702b1f7a2a362c7b80688
Ephraim <ephraim.pepe@gmail.com>
2013-12-30 16:28:11 +0100

    Ridistribuzione

--- src/WSP/PromoBundle/Resources/public/js/vertical_justify.js
+++ src/WSP/PromoBundle/Resources/public/js/vertical_justify.js
@@ -12,8 +12,8 @@ var VerticalJustify = function() {
         //main function to initiate the module
         init: function() {
             div_logo = $('#logo').closest('div');
-            logo_extra_margin = 25; //parseInt(div_logo.css('padding-top')) + 10;
-            logo_top_margin = 30; //parseInt(div_logo.css('margin-top'));
+            logo_extra_margin = parseInt(div_logo.css('padding-top')) + 10;
+            logo_top_margin = parseInt(div_logo.css('margin-top'));
             logo_height = $('#logo').height() - 10;
             tot_height = $('#content').height() + parseInt($('#content').css('padding-bottom')) + parseInt($('#content').css('margin-bottom')) +
                          $('#countdown').height() + parseInt($('#countdown').css('padding-top')) + parseInt($('#countdown').css('margin-top'));
@@ -26,7 +26,7 @@ var VerticalJustify = function() {
             var _tot, _margin, _area = 0;
 
             _tot = windows_height - tot_height - logo_height;
-            _margin = _tot / 4;
+            _margin = _tot / 3;
 
             if(_margin < logo_top_margin + logo_extra_margin) {
                 _margin = logo_top_margin + logo_extra_margin;
