7e2a2812659ababae10e7d0c5cdec490c682ffba
Ephraim <ephraim.pepe@gmail.com>
2014-01-08 15:20:29 +0100

    refactoring css e add facebook e youtube

--- src/WSP/PromoBundle/Resources/public/js/coming-soon.js
+++ src/WSP/PromoBundle/Resources/public/js/coming-soon.js
@@ -3,9 +3,7 @@ var ComingSoon = function () {
     return {
         //main function to initiate the module
         init: function () {
-
-            var austDay = new Date(2014, 6, 6, 0, 0, 0, 0);
-            //austDay = new Date(austDay.getFullYear() + 1, 1 - 1, 26);
+            var austDay = new Date(2014, 6, 9);
             $('#defaultCountdown').countdown({until: austDay});
             $('#year').text(austDay.getFullYear());
         }
