f87eac4739120eef378615c3e8dfa150c8df56ab
Ephraim <ephraim.pepe@gmail.com>
2014-01-30 10:32:47 +0100

    fix data

--- src/WSP/PromoBundle/Resources/public/js/coming-soon.js
+++ src/WSP/PromoBundle/Resources/public/js/coming-soon.js
@@ -2,7 +2,7 @@ var ComingSoon = function () {
     return {
         //main function to initiate the module
         init: function () {
-            var austDay = new Date(2014, 5, 9);
+            var austDay = new Date(2014, 5, 10);
             $('#defaultCountdown').countdown({until: austDay});
             $('#year').text(austDay.getFullYear());
         }
